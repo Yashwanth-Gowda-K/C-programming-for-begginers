8b66bbfed6b6fdb749a1ac7723aff88210648e13
Yashavanth Gowda K K <yashyash808859@gmail.com>
2024-11-13 19:14:32 +0530

    Add files via upload

---
+++ Sum of Natural Numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n,sum=0;
+    printf("Enter a postive number: ");
+    scanf("%d", &n);
+
+    if (n<1){
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+    for   {
+        sum +=i;
+ 
+    }
+
+    printf("The sum of natural numbers up to %d is %d.\n" , n , sum);
+    return 0;
+}
